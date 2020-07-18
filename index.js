5af24fbd69bcee02854336f0d580ec6d860cb4b2
David Castro Castaneda <dcastrocast@gmail.com>
2020-07-17 22:22:12 -0700

    asciiart-logo depedency and fucntion add

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const { prompt } = require('inquirer');
+const logo = require('asciiart-logo');
+const db = require('./db');
+require('console.table');
 
+init();
+
+// Display logo text, load main prompts
+function init() {
+  const logoText = logo({ name: 'Employee Manager' }).render();
+
+  console.log(logoText);
+}
