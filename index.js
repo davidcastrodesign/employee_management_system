0d506d0a728959f614c524000f94f94ab9a5594f
David Castro Castaneda <dcastrocast@gmail.com>
2020-07-18 01:44:28 -0700

    prompt add

--- index.js
+++ index.js
@@ -10,4 +10,50 @@ function init() {
   const logoText = logo({ name: 'Employee Manager' }).render();
 
   console.log(logoText);
+
+  loadMainPrompts();
+}
+
+async function loadMainPrompts() {
+  const { choice } = await prompt([
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'What would you like to do?',
+      choices: [
+        {
+          name: 'Add Department',
+          value: 'ADD_DEPARTMENT',
+        },
+        {
+          name: 'Add Role',
+          value: 'ADD_ROLE',
+        },
+        {
+          name: 'Add Employee',
+          value: 'ADD_EMPLOYEE',
+        },
+        {
+          name: 'View All Departments',
+          value: 'VIEW_DEPARTMENTS',
+        },
+        {
+          name: 'View All Roles',
+          value: 'VIEW_ROLES',
+        },
+        {
+          name: 'View All Employees By Manager',
+          value: 'VIEW_EMPLOYEES_BY_MANAGER',
+        },
+        {
+          name: 'Update Employee Role',
+          value: 'UPDATE_EMPLOYEE_ROLE',
+        },
+        {
+          name: 'Quit',
+          value: 'QUIT',
+        },
+      ],
+    },
+  ]);
 }
